f0504429b0c8635531fe2b3210927a5453f75d4b
AKBarcenas <akbarcen@ucsd.edu>
2015-12-28 01:23:07 -0800

    Initial Commit

---
+++ Label Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Label Layout
+//
+//  Created by Alex on 12/28/15.
+//  Copyright © 2015 Alex Barcenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
